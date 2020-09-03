abe81e9cef18bbdf4c506654530d98c0e7effd67
PankhuriBansal <66808553+PankhuriBansal@users.noreply.github.com>
2020-09-03 15:41:06 +0530

    Updates

---
+++ precache-manifest.5f03103f9d4f2a9b2bd48215d2b11a20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76b2a48fa3ba41317e4f32a25866166f",
+    "url": "/creditmanagement.github.io/index.html"
+  },
+  {
+    "revision": "d2177f79fddd98e712d1",
+    "url": "/creditmanagement.github.io/static/css/main.ba41e3c0.chunk.css"
+  },
+  {
+    "revision": "0db090e9e350ae1bdb3a",
+    "url": "/creditmanagement.github.io/static/js/2.e9423d7f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/creditmanagement.github.io/static/js/2.e9423d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2177f79fddd98e712d1",
+    "url": "/creditmanagement.github.io/static/js/main.7fb39e50.chunk.js"
+  },
+  {
+    "revision": "06ca01d09578534efe5d",
+    "url": "/creditmanagement.github.io/static/js/runtime-main.9be4344a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/creditmanagement.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
